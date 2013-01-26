29145bf6db7c8d83fb79bd22ff8f321306b902ba
Michael Robinson <mikelrob@hotmail.com>
2013-01-26 14:50:48 +0000

    Revert "Revert "base actor factory class""
    This reverts commit c61592b60fbbf76084ab54ab30c3110cec83e71b.

---
+++ Cardiac Castle/Cardiac Castle/Factories/ggjActorFactory.h
@@ -0,0 +1,20 @@
+//
+//  ggjActorFactory.h
+//  Cardiac Castle
+//
+//  Created by Andrew Boyd on 26/01/2013.
+//  Copyright (c) 2013 GGJ13. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ggjActor.h"
+
+@interface ggjActorFactory : NSObject
+
+@property CGFloat distTravelled;
+@property CGFloat baseSpawnProb;
+@property NSUInteger numActorsAlive;
+@property CGFloat heartRate;
+@property (strong) ggjActor *actorToSpawn;
+
+@end
