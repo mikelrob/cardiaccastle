c3f2743a53bb335ce8b78257497074873f72bf94
Michael Robinson <mikelrob@hotmail.com>
2013-01-26 14:10:45 +0000

    created actor calss

---
+++ Cardiac Castle/Cardiac Castle/ggjActor.h
@@ -0,0 +1,13 @@
+//
+//  ggjActor.h
+//  Cardiac Castle
+//
+//  Created by Michael Robinson on 26/01/2013.
+//  Copyright (c) 2013 GGJ13. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ggjActor : NSObject
+
+@end
---
+++ Cardiac Castle/Cardiac Castle/ggjActor.m
@@ -0,0 +1,13 @@
+//
+//  ggjActor.m
+//  Cardiac Castle
+//
+//  Created by Michael Robinson on 26/01/2013.
+//  Copyright (c) 2013 GGJ13. All rights reserved.
+//
+
+#import "ggjActor.h"
+
+@implementation ggjActor
+
+@end
